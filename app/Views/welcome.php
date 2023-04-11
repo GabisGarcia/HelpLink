2d0ecfef4a751be297b54e5eca208b0af2f410eb
Gabriele Garcia <garciagabriele@hotmail.com>
2023-04-11 19:49:02 -0300

    commit

---
+++ app/Views/welcome.php
@@ -0,0 +1,13 @@
+<?php
+    $this->extend('header');
+
+    $this->section('title');
+
+        echo "HelpLink";
+
+    $this->endSection();
+
+    $this->section('content');
+        echo "<p>alo</p>";
+    $this->endSection();
+?>
