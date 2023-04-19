2960617ad519dd5c3186a29914fb6ca1763cc145
LettSly <snches.lauren@gmail.com>
2023-04-18 22:19:49 -0300

    botaozinho navegar

--- app/Views/header.php
+++ app/Views/header.php
@@ -6,6 +6,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
     <title><?= $this->renderSection('title')?></title>
+    <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">   
     <link rel="stylesheet" type="text/css" href="../../public/index.css"/>
     <script src="https://cdn.jsdelivr.net/npm/swiffy-slider@1.6.0/dist/js/swiffy-slider.min.js" crossorigin="anonymous" defer></script>
     <link href="https://cdn.jsdelivr.net/npm/swiffy-slider@1.6.0/dist/css/swiffy-slider.min.css" rel="stylesheet" crossorigin="anonymous">
@@ -16,6 +18,13 @@
         .btnPink{
             background: #FDCFDF;
         }
+
+        .btnPink2{
+            background: #FDCFDF;
+            width:120px;
+            height:43px;
+        }
+        
         body {
         color: #5a5a5a;
         }
@@ -29,13 +38,19 @@
         .carousel-item {
             height: 32rem;
         }
-        .carousel-item > img {
+        .img {
             position: absolute;
             top: 0;
             left: 0;
             min-width: 100%;
             height: 32rem;
         }
+
+        .slider-container img{
+            width: 100%;
+            height: 400px;
+        }
+
         .marketing .col-lg-4 {
             margin-bottom: 1.5rem;
             text-align: center;
@@ -86,28 +101,34 @@
             <form class="form-inline">
                 <input class="form-control mr-sm-2" type="search" placeholder="Pesquisar" aria-label="Search">
                 <button class="btn btnPink" type="submit">
-                <img src="https://cdn3.iconfinder.com/data/icons/mix-and-miscellaneous/93/handrawn_search_magnify-512.png" alt="Pesquisar" height="25" width="25"><img> 
+                <img src="https://cdn3.iconfinder.com/data/icons/mix-and-miscellaneous/93/handrawn_search_magnify-512.png" alt="Pesquisar" height="25" width="25">
                 </button>
-            
             </form>
                 <ul class="nav justify-content-end">
-
                     <li class="nav-item">
-                        <a class="nav-link active" href="#">Quem somos nós?</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Desconectar</a>
-                    </li>
-                    <li class="nav-item">
-                        <button class="btn btnPink" type="submit">
-                            Meu perfil
-                            <img src="https://cdn0.iconfinder.com/data/icons/eon-social-media-contact-info-2/32/user_people_person_users_man-256.png" alt="Pesquisar" height="25" width="25"><img> 
-                        </button>
+                        <div class="w3-dropdown-click">
+                           <button class="btn btnPink2" onclick="myFunction()">
+                                 <img src="https://cdn0.iconfinder.com/data/icons/eon-social-media-contact-info-2/32/user_people_person_users_man-256.png" alt="Pesquisar" height="25" width="25"><img>  <i class="fa fa-caret-down"></i>
+                            </button>
+                            <div id="demo" class="w3-dropdown-content w3-bar-block w3-card-4">
+                                <a href="#" class="w3-bar-item w3-button">Meu Perfil</a>
+                                <a href="#" class="w3-bar-item w3-button">Configurações</a>
+                                <a href="#" class="w3-bar-item w3-button">Sair</a>
+                            </div>
                     </li>
                 </ul>
-
-                
         </nav>
+
+        <script>
+            function myFunction() {
+                var x = document.getElementById("demo");
+                    if (x.className.indexOf("w3-show") == -1) {
+                        x.className += " w3-show";
+                    } else { 
+                        x.className = x.className.replace(" w3-show", "");
+                    }
+            }
+        </script>
     </header>
 <body>
     <?= $this->renderSection('content');?>
