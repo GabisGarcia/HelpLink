50aa7982383c4d8dd4765711884682ca20a534e2
noozajureg <Gabriel-rodrigues-martins@hotmail.com>
2023-05-09 21:39:49 -0300

    listar pesquisa por tag ou por pesquisa e tag

--- app/Models/PostModel.php
+++ app/Models/PostModel.php
@@ -8,14 +8,21 @@
     {
         protected $table = 'POST';
         protected $primaryKey = 'ID_POST';
-        //protected $foreignkey = ['ID_CONTA']; PESQUISAR SOBRE...
         protected $useAutoIncrement = true;
         protected $allowedFields = ['TITULO', 'DESCRICAO', 'VALOR', 'DOACAO', 'CONTATO', 'ID_CONTA'];
         protected $returnType = 'object';
 
-        public function listarPesquisa($pesquisa)
+        public function listarPesquisa($pesquisa, $tag)
         {
-            $resultado = $this->db->query('SELECT * FROM post WHERE TITULO LIKE "'. $pesquisa . '"');
+
+
+            if ($pesquisa != ''){
+                $resultado = $this->db->query('SELECT * FROM post WHERE TITULO LIKE "'. $pesquisa . '"');
+            }elseif($tag != ''){
+                $resultado = $this->db->query('SELECT * FROM POST WHERE ID_POST IN (SELECT ID_POST FROM post_tag WHERE ID_TAG = '. $tag .')');
+            }else{
+                $resultado = $this->db->query('SELECT * FROM POST WHERE TITULO LIKE "'. $pesquisa . '" AND ID_POST IN (SELECT ID_POST FROM post_tag WHERE ID_TAG = '. $tag .')');
+            }
 
             foreach ($resultado->getResult('array') as $row) {
                 echo $row['TITULO'];
