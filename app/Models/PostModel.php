b95bb9345672d970ad5a1613c57e966369bfbc26
noozajureg <gabriel-rodrigues-martins@hotmail.com>
2023-05-16 22:24:04 -0300

    postmodel v4

--- app/Models/PostModel.php
+++ app/Models/PostModel.php
@@ -14,31 +14,57 @@
 
         private function listarRes($resultado)
         {
-            foreach ($resultado->getResult('array') as $row) {
-                if($row == []){
-                    echo "<h1>Não há post</h1>";
+            if($resultado->getRow() == null){
+                echo "<h1>Não há post</h1>";
+            }else{  
+                foreach ($resultado->getResult('array') as $row) {
+                    // var_dump($resultado);
+                    echo $row['TITULO'];
+                    echo "<br>";
+                    echo $row['DESCRICAO'];
+                    echo "<br>";
+                    
                 }
-                echo $row['TITULO'];
-                echo "<br>";
-                echo $row['DESCRICAO'];
-                echo "<br>";
-                var_dump($resultado);
             }
         }
+        
 
         public function listarPesquisa($data)
         {
-            $pesquisa = ''; // $data['Pesquisa'];
-            $tag = '1';// $data['Tags'];
+            $pesquisa = 'carro'; // $data['Pesquisa'];
+            $tags = [1, 2];
+            $consulta = "ID_TAG = ";
 
-            if(isset($pesquisa) && isset($tags)){
-                $resultado = $this->db->query('SELECT * FROM POST WHERE ID_POST IN (SELECT ID_POST FROM post_tag WHERE ID_TAG = '. $tag .' AND TITULO LIKE "'. $pesquisa . '")');
+
+            if(sizeof($tags) > 1) {
+                for($i = 0; $i < sizeof($tags); $i++) {
+                    if($i == (sizeof($tags) - 1)) {
+                        $consulta .= "{$tags[$i]} ";
+                    } else {
+                        $consulta .= "{$tags[$i]} AND ";
+
+                    }
+                }
+            } else if(sizeof($tags) == 1){
+                $consulta .= "{$tags[0]} ";
+            }else{
+                $consulta = $consulta;
+            }
+            
+            if($pesquisa != '' && $tags != []){
+                echo "tem dois<br>";
+                echo 'SELECT * FROM POST WHERE ID_POST IN (SELECT ID_POST FROM post_tag WHERE ' . $consulta .' AND TITULO LIKE "%'. $pesquisa . '%");';
+                $resultado = $this->db->query('SELECT * FROM POST WHERE ID_POST IN (SELECT ID_POST FROM post_tag WHERE ' . $consulta .' AND TITULO LIKE "%'. $pesquisa . '%");');
                 $this->listarRes($resultado);
-            }elseif($pesquisa == '' && $tag != ''){
-                $resultado = $this->db->query('SELECT * FROM POST WHERE ID_POST IN (SELECT ID_POST FROM post_tag WHERE ID_TAG = '. $tag .')');
+            }elseif($pesquisa == '' && $tags != []){
+                echo "tem tag<br>";
+                echo 'SELECT * FROM POST WHERE ID_POST IN (SELECT ID_POST FROM post_tag WHERE ' . $consulta .');';
+                $resultado = $this->db->query('SELECT * FROM POST WHERE ID_POST IN (SELECT ID_POST FROM post_tag WHERE ' . $consulta .');');
                 $this->listarRes($resultado);
-            }elseif($pesquisa != '' && $tag == ''){
-                $resultado = $this->db->query('SELECT * FROM POST WHERE TITULO LIKE "'. $pesquisa . '"');
+            }elseif($pesquisa != '' && $tags == []){
+                echo "tem pesquisa<br>";
+                echo 'SELECT * FROM POST WHERE TITULO LIKE "'. $pesquisa . '";';
+                $resultado = $this->db->query('SELECT * FROM POST WHERE TITULO LIKE "'. $pesquisa . '";');
                 $this->listarRes($resultado);
             }else{
                 echo "<h1>Não há post</h1>";
