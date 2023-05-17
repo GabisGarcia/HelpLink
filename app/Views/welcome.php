6f87552431b8419da12c8bf3dced8d210a886d3b
GabisGarcia <garciagabriele@hotmail.com>
2023-05-17 19:57:34 -0300

    title

--- app/Views/welcome.php
+++ app/Views/welcome.php
@@ -4,14 +4,12 @@
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Document</title>
+  <title>HelpLink</title>
+  <link rel="icon" href="<?= base_url()?>/favicon.ico" type="image/x-icon">
 </head>
 <body>
         <meta charset="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
-        <meta name="description" content="" />
-        <meta name="author" content="" />
-        <title>Meu perfil</title>
         <!-- Font Awesome icons (free version)-->
         <script src="https://use.fontawesome.com/releases/v6.3.0/js/all.js" crossorigin="anonymous"></script>
         <!-- Google fonts-->
