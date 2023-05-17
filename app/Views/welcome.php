41d229db4663f539226c03e45af99d5b295fa730
LettSly <snches.lauren@gmail.com>
2023-05-17 20:36:07 -0300

    Correções no welcome

--- app/Views/welcome.php
+++ app/Views/welcome.php
@@ -4,14 +4,9 @@
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Document</title>
-</head>
-<body>
-        <meta charset="utf-8" />
+  <title>HelpLink</title>
+  <link rel="icon" href="<?= base_url()?>/favicon.ico" type="image/x-icon">
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
-        <meta name="description" content="" />
-        <meta name="author" content="" />
-        <title>Meu perfil</title>
         <!-- Font Awesome icons (free version)-->
         <script src="https://use.fontawesome.com/releases/v6.3.0/js/all.js" crossorigin="anonymous"></script>
         <!-- Google fonts-->
