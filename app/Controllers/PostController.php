63608a7ee7d0aef0cf410ff85660fecc19c828a0
Noozajureg <Gabriel-rodrigues-martins@hotmail.com>
2023-06-18 21:00:37 -0300

    listarpostusuario

--- app/Controllers/PostController.php
+++ app/Controllers/PostController.php
@@ -71,6 +71,12 @@ class PostController extends BaseController
 
     }
 
+    public function listarPostUsuario($idUsuario) {
+        $idUsuario = $this->request->getPost('ID_USUARIO');
+        $posts = $this->PostModel->listarPostUsuario($idUsuario);
+        return $posts;
+    }
+
     public function editar()
     {
         $qtsTags = sizeof($this->TagModel->getTags());
